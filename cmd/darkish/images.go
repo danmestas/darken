835ee54843822fabb2d224ac955afff89ecccb46
Dan Mestas <93289084+danmestas@users.noreply.github.com>
2026-04-27 08:33:50 -0500

    feat(cli): implement darkish images (wraps make -C images)

--- cmd/darkish/images.go
+++ cmd/darkish/images.go
@@ -1,5 +1,16 @@
 package main
 
-import "errors"
+import (
+	"os"
+	"os/exec"
+)
 
-func runImages(args []string) error { return errors.New("not implemented") }
+func runImages(args []string) error {
+	if len(args) == 0 {
+		args = []string{"all"}
+	}
+	c := exec.Command("make", append([]string{"-C", "images"}, args...)...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return c.Run()
+}
